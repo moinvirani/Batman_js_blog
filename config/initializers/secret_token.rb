4fcf790b9ee2a9089caa08a5f6abc8cba269ad4f
Moin Virani <moin.virani@gmail.com>
2013-12-10 14:26:19 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BatmanJsBlog::Application.config.secret_key_base = '5a210282ae78e9b5ff6e89e7c8541e0026b2f428cd6e360c9f4b6e9547660080a8dc14e4949dd491960a70e491c64524bab804dbbd8c117dea02cd41e137fd87'
